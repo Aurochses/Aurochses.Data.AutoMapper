f01afcff8b84ecd1b70364dd8f5338cb8c5080d8
Vladimir Rodchenko <vladimir@aurochses.com>
2017-07-31 01:49:13 +0300

    initial code

---
+++ test/Aurochses.Data.AutoMapper.Tests/Fakes/FakeDestination.cs
@@ -0,0 +1,8 @@
+﻿namespace Aurochses.Data.AutoMapper.Tests.Fakes
+{
+    public class FakeDestination
+    {
+        public int Id { get; set; }
+        public string DestinationName { get; set; }
+    }
+}
---
+++ test/Aurochses.Data.AutoMapper.Tests/MapperTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Aurochses.Data.AutoMapper.Tests.Fakes;
+using Xunit;
+
+namespace Aurochses.Data.AutoMapper.Tests
+{
+    public class MapperTests
+    {
+        [Fact]
+        public void Map_Success()
+        {
+            // Arrange
+            var source = new List<FakeSource>
+            {
+                new FakeSource {Id = 1, SourceName = "FirstSourceName"},
+                new FakeSource {Id = 2, SourceName = "SecondSourceName"},
+                new FakeSource {Id = 3, SourceName = "ThirdSourceName"}
+            };
+
+            global::AutoMapper.Mapper.Initialize(cfg => cfg.CreateMap<FakeSource, FakeDestination>());
+
+            var mapper = new Mapper();
+
+            var expected = new List<FakeDestination>
+            {
+                new FakeDestination {Id = 1},
+                new FakeDestination {Id = 2},
+                new FakeDestination {Id = 3}
+            };
+
+            // Act
+            var result = mapper.Map<FakeDestination>(source.AsQueryable()).ToList();
+
+            // Assert
+            for (var i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].Id, result[i].Id);
+                Assert.Equal(expected[i].DestinationName, result[i].DestinationName);
+            }
+        }
+    }
+}
